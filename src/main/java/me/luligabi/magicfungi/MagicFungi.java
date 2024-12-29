754ad10101fea792d5e34e79402af85fead966b4
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-12-29 18:20:16 +0000

    Setup base

---
+++ src/main/java/me/luligabi/magicfungi/MagicFungi.java
@@ -0,0 +1,13 @@
+package me.luligabi.magicfungi;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(MagicFungi.MODID)
+public class MagicFungi {
+    public static final String MODID = "magicfungi";
+
+    public MagicFungi(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
